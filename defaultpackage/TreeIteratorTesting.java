a5a4cc7102c9e5e721438c5fdcfc0b75090a5664
James543R <james.libby@maine.edu>
2023-10-25 14:45:52 -0400

    created TreeIteratorTesting.java

---
+++ defaultpackage/TreeIteratorTesting.java
@@ -0,0 +1,13 @@
+package defaultpackage;
+public class TreeIteratorTesting {
+	
+	public TreeIteratorTesting(BinarySearchTree bst, ITreeIterator treeIter) {
+		
+		
+	}
+	
+	public boolean checkOrder() {
+		
+		return false;
+	}
+}
