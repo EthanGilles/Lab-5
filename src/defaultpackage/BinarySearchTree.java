01f8885f24d2d4cbdb837931e853bfaa8ca48d66
EthanGilles <ehgilles01@gmail.com>
2023-10-25 14:18:48 -0400

    Task One
     BinarySearchTree Add Method

--- src/defaultpackage/BinarySearchTree.java
+++ src/defaultpackage/BinarySearchTree.java
@@ -2,6 +2,20 @@ package defaultpackage;
 
 public abstract class BinarySearchTree {
 	public int size;
+	public Node root;
+	
+	public static class Node {
+		
+		int data;
+		Node left, right;
+		
+		public Node(int data) {
+			this.data = data;
+			left = null;
+			right = null;
+		}
+	}
+	
 	public abstract boolean add(int data);
 	public int getSize() {
 		return size;
--- src/defaultpackage/myBinarySearchTree.java
+++ src/defaultpackage/myBinarySearchTree.java
@@ -1,5 +1,37 @@
 package defaultpackage;
 
 public class myBinarySearchTree extends BinarySearchTree {
+	
+	/* TASK ONE
+	 * ETHAN GILLES
+	 * 
+	 */
+	
+	public myBinarySearchTree() {
+		super.size = 0;
+	}
+
+
+	//public access method for add
+	public boolean add(int data) {
+		return add(data, super.root);
+	}
+	
+	//private recursive helper method
+	private boolean add(int data, Node node) {
+		if(node == null) {
+			node = new Node(data);
+			super.size++;
+		}
+		else if(data == node.data) {
+			return false;
+		}
+		else if(data < node.data)
+			return add(data, node.left);
+		else if(data > node.data)
+			return add(data, node.right);
+		
+		return true;
+	}
 
 }
